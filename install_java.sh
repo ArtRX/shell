ac793fd4dbdefcddd9cc90a68ce2c46a6c8ee254
kkorotaev <k.u.korotaev@gmail.com>
2018-10-29 22:07:58 +0300

    java install

--- install_java.sh
+++ install_java.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
-sudo apt install openjdk-$0-jdk
+echo 'openjdk-'+$0+'-jre'
+sudo apt-get install openjdk-$0-jdk
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
 export PATH=$PATH:$JAVA_HOME/bin
 java --version
